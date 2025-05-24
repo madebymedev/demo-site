90b112b78a888f0cc38bfb2b113af89c562c7fac
madebymedev <hansanders568@gmail.com>
2025-05-24 17:44:08 +0200

    updating css syntax

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="relative antialiased text-white font-[Quicksand] bg-[url(/hp-bg.jpg)] bg-cover bg-top bg-no-repeat">
+      <body className="relative antialiased text-white font-[Quicksand] bg-[url(/hp-bg.jpg)] bg-no-repeat">
         <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
         <div className="relative z-10">{children}</div>
       </body>
