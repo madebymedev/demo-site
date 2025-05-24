17077c2fac2851a8a71eccca4aa37faaab5df643
madebymedev <hansanders568@gmail.com>
2025-05-24 17:33:38 +0200

    Fix tailwind syntax

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,9 +26,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`relative ${geistSans.variable} ${geistMono.variable} ${quicksand.variable} antialiased text-white bg-[url(/hp-bg.jpg)] bg-cover bg-top bg-no-repeat`}
-      >
+      <body className="relative antialiased text-white font-[Quicksand] bg-[url(/hp-bg.jpg)] bg-cover bg-top bg-no-repeat">
         <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
         <div className="relative z-10">{children}</div>
       </body>
