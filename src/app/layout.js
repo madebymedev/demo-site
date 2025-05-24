901394e3bced86a535ac2741a8e7c6310f5de162
madebymedev <hansanders568@gmail.com>
2025-05-24 18:13:20 +0200

    change of background

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="relative antialiased text-white font-[Quicksand] bg-[url(/hp-bg.jpg)] bg-center bg-[length:100vw_100vh]">
+      <body className="relative antialiased text-white font-[Quicksand] bg-[url(/gradient-bg.jpg)] bg-center bg-[length:100vw_100vh]">
         <div className="absolute inset-0 bg-black/30 backdrop-blur-sm"></div>
         <div className="relative z-10">{children}</div>
       </body>
